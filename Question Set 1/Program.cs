8fb9fb472b09712f11566de8a8dd2b5b94e3661a
arunk2309 <arunachalamkathiresan.95@gmail.com>
2023-11-24 13:03:11 +0530

    Commit 2

--- Question Set 1/Program.cs
+++ Question Set 1/Program.cs
@@ -56,10 +56,36 @@ namespace Question_Set_1
             //Console.WriteLine("The Remainder is:" + R);
 
             // No 6
-            Console.WriteLine("Enter the Number:");
+            //Console.WriteLine("Enter the Number:");
+            //int number = int.Parse(Console.ReadLine());
+            //double square = Math.Pow(number, 2);
+            //Console.WriteLine("THe Square of a Given number is:" + square);
+
+            // No 7
+            //Console.WriteLine("Enter the Number:");
+            //int number = int.Parse(Console.ReadLine());
+            //double sqrt = Math.Sqrt(number);
+            //Console.WriteLine("The Square root of" + number + "is" + sqrt);
+
+            // No 8
+            //Console.WriteLine("Enter the Breadth:");
+            //int breadth = int.Parse(Console.ReadLine());
+            //Console.WriteLine("Enter the Height:");
+            //int height = int.Parse(Console.ReadLine());
+            //double area = ((0.5) * (breadth * height));
+            //Console.WriteLine("The Area of Traingle is:" + area);
+
+            //No 9
+            //Console.WriteLine("Enther the Kilometer:");
+            //int km = int.Parse(Console.ReadLine());
+            //double value = ((km) * (0.621371));
+            //Console.WriteLine("Mile of" + km + "Kilometer:" + value);
+
+            // No 10
+            Console.WriteLine("Enter the Celsius:");
             int number = int.Parse(Console.ReadLine());
-            double square = Math.Pow(number, 2);
-            Console.WriteLine("THe Square of a Given number is:" + square);
+            double convertedvalue = ((number * (1.8)) + 32);
+            Console.WriteLine("The Celsius" + number + "Farenheit is:" + convertedvalue);
         }
     }
 }
