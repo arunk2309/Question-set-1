cd4c261eed344d1b196c0aa8b5b6efeaea5ea78f
arunk2309 <arunachalamkathiresan.95@gmail.com>
2023-11-24 12:26:26 +0530

    Commit Set1

--- Question Set 1/Program.cs
+++ Question Set 1/Program.cs
@@ -20,12 +20,46 @@ namespace Question_Set_1
             //Console.WriteLine("Product of Two numbers is:" + n1 * n2);
 
             //No 2
-            Console.WriteLine("Enter the value of a:");
-            int a = int.Parse(Console.ReadLine());
-            Console.WriteLine("Enter the value of B:");
-            int b = int.Parse(Console.ReadLine());
-            double total = Math.Pow(a, b);
-            Console.WriteLine("The raised integer of" + a + "and" + b + "is" + total);
+            //Console.WriteLine("Enter the value of a:");
+            //int a = int.Parse(Console.ReadLine());
+            //Console.WriteLine("Enter the value of B:");
+            //int b = int.Parse(Console.ReadLine());
+            //double total = Math.Pow(a, b);
+            //Console.WriteLine("The raised integer of" + a + "and" + b + "is" + total);
+
+            //No 3
+            //Console.WriteLine("Enter the Length:");
+            //int length = int.Parse(Console.ReadLine());
+            //Console.WriteLine("Enthe the Width:");
+            //int width = int.Parse(Console.ReadLine());
+            //Console.WriteLine("Enter the Height:");
+            //int height = int.Parse(Console.ReadLine());
+            //int Volume = length * width * height;
+            //Console.WriteLine("The volume of the tank is:" + Volume);
+
+            // No 4
+            //Console.WriteLine("Enter the Radius:");
+            //int radius = int.Parse(Console.ReadLine());
+            //double perimeter = 2 * Math.PI * radius;
+            //Console.WriteLine("The Perimeter of the Circle is:" + perimeter);
+
+
+
+            // No 5
+            //Console.WriteLine("Enter the Numbertor:");
+            //int numerator = int.Parse(Console.ReadLine());
+            //Console.WriteLine("Emter the Denominator:");
+            //int denominator = int.Parse(Console.ReadLine());
+            //int Q = numerator / denominator;
+            //int R = numerator % denominator;
+            //Console.WriteLine("The Quotient is:" + Q);
+            //Console.WriteLine("The Remainder is:" + R);
+
+            // No 6
+            Console.WriteLine("Enter the Number:");
+            int number = int.Parse(Console.ReadLine());
+            double square = Math.Pow(number, 2);
+            Console.WriteLine("THe Square of a Given number is:" + square);
         }
     }
 }
